78b1aa5fb0c968aad20ede01500c2a074e873f9a
shravani-1234 <120002464+shravani-1234@users.noreply.github.com>
2025-04-20 17:08:59 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import TradeForm from "./Tradeform.js";
+import TradeHistory from "./TradeHistory";
+
+function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <TradeForm />
+      <TradeHistory />
+    </div>
+  );
+}
+
+export default App;
+
+
+
